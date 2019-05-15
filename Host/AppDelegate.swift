3c1dd6e6d651ac037bf6047445998e3a8b103aab
Rodolfo Pujol Luna <rodolfop@digital2go.com>
2019-05-14 17:44:40 -0700

    Add Unit tests

---
+++ Host/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Host
+//
+//  Created by Jigar Thakkar on 19/12/18.
+//  Copyright © 2018 Locally. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
---
+++ Host/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  Host
+//
+//  Created by Jigar Thakkar on 19/12/18.
+//  Copyright © 2018 Locally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
